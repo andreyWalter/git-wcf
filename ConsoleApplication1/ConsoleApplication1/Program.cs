b82ba5089517cc54f0c5a04bda976ac7f24940bd
Andrey Tarariew <ter111111@yandex.ru>
2016-12-13 18:28:54 +0200

    Commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            ///
+            Uri adress = new Uri("http://127.0.0.1:8000/IService");
+
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress endpoint = new EndpointAddress(adress);
+
+            ChannelFactory<IService> factory = new ChannelFactory<IService>(binding, endpoint);
+            IService chanel = factory.CreateChannel();
+            for(;;)
+            {
+                try {chanel.sayHello("Hello WCF"); }
+                catch(Exception r) {  }
+                Thread.Sleep(100);
+            }
+           
+                        
+            Console.ReadLine();
+        }
+    }
+    [ServiceContract]
+    interface IService
+    {
+        
+        [OperationContract]
+        void sayHello(string enter);
+    }
+    class Service : IService
+    {
+        public void sayHello(string enter)
+        {
+            Console.WriteLine("Hello there"+enter);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            // Uri adress = new Uri("http://127.0.0.1:8000/IService1");
+
+            //BasicHttpBinding binding = new BasicHttpBinding();
+
+            Type contract = typeof(IService);
+
+            ServiceHost host = new ServiceHost(typeof(IService), new Uri("http://127.0.0.1:8000/IService"));
+            host.AddServiceEndpoint(typeof(IService), new BasicHttpBinding(), "");
+            host.Open();
+
+            Console.WriteLine("Solution is ready");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+    [ServiceContract]
+    interface IService
+    {
+        [OperationContract]
+        void sayHello(string enter);
+    }
+    class Service : IService
+    {
+        public void sayHello(string enter)
+        {
+            Console.WriteLine("Hello there" + enter);
+        }
+    }
+}
